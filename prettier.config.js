f584e69ef11b30853afafd823952d7b71548b4c7
harshv5094 (Local User) <harshv5094@gmail.com>
2024-03-13 16:18:28 +0530

    feat: Initial Commit

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/** @type {import('prettier').Config} */
+
+const config = {
+  tabWidth: 2,
+  singleQuote: true,
+  semi: false,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  endOfLine: 'lf'
+}
+
+module.exports = config
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,29 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+      colors: {
+        lightRed: 'hsl(0, 100%, 67%)',
+        orangeYellow: 'hsl(39, 100%, 56%)',
+        greenTeal: 'hsl(166, 100%, 37%)',
+        cobaltBlue: 'hsl(234, 85%, 45%)',
+        white: 'hsl(0, 0%, 100%)',
+        paleBlue: 'hsl(221, 100%, 96%)',
+        lightLavender: 'hsl(241, 100%, 89%)',
+        darkGrayBlue: 'hsl(224, 30%, 27%)',
+        lightSlateBlue: 'hsl(252, 100%, 67%)',
+        lightRoyalBlue: 'hsl(241, 81%, 54%)',
+        violetBlue: 'hsla(256, 72%, 46%, 1)',
+        persianBlue: 'hsla(241, 72%, 46%, 0)'
+      }
+    }
   },
-  plugins: [],
-};
-export default config;
+  plugins: []
+}
+export default config
